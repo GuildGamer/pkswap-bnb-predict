de407c11b162c0136900df5f99dc675d32bf93c7
Steven Umar <tobi4steve@gmail.com>
2022-06-13 10:52:28 +0100

    update

--- src/index.py
+++ src/index.py
@@ -7,33 +7,35 @@ today = date.today()
 # d1 = today.strftime("%Y-%m-%d")
 d1 = datetime.now()
 end_date = d1
-d2 = datetime.now() - timedelta(minutes = 59 * 24 * 60)
+d2 = datetime.now() - timedelta(days = 5)
+# d2 = date.today() - timedelta(days=59)
+# print("DAY 2 {d2}")
 # d2 = d2.strftime("%Y-%m-%d")
 start_date = d2
 
 data = yf.download('BNB-USD', 
-                      start=start_date, 
-                      end=end_date, 
-                      progress=False,
-                      interval='5m')
+    start=start_date, 
+    end=end_date, 
+    progress=False,
+    interval="1h")
 data["Date"] = data.index
 data = data[["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"]]
 data.reset_index(drop=True, inplace=True)
 
 # print(data.tail())
-# print(data.shape)
+print(data.shape)
 
 # visualize the change in bnb prices till today by using a candlestick chart
 
-# import plotly.graph_objects as go
-# figure = go.Figure(data=[go.Candlestick(x=data["Date"],
-#                                         open=data["Open"], 
-#                                         high=data["High"],
-#                                         low=data["Low"], 
-#                                         close=data["Close"])])
-# figure.update_layout(title = "Bitcoin Price Analysis", 
-#                      xaxis_rangeslider_visible=False)
-# figure.show()
+import plotly.graph_objects as go
+figure = go.Figure(data=[go.Candlestick(x=data["Date"],
+                                        open=data["Open"], 
+                                        high=data["High"],
+                                        low=data["Low"], 
+                                        close=data["Close"])])
+figure.update_layout(title = "BNB Price Analysis", 
+                     xaxis_rangeslider_visible=False)
+figure.show()
 
 # Viewing the correlation betwween the "Close" column and the other columns
 # correlation = data.corr()
@@ -42,7 +44,7 @@ data.reset_index(drop=True, inplace=True)
 # AutoTS is a Tme Series library for Python
 from autots import AutoTS
 
-model = AutoTS(forecast_length=3, frequency='infer', ensemble='simple')
+model = AutoTS(forecast_length=1, frequency='infer', ensemble='simple')
 
 # Creating the model
 model = model.fit(data, date_col='Date', value_col='Close', id_col=None)
